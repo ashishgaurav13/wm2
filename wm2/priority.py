e419f40c328caf9eae173d6e2c784aa827a932b8
Ashish Gaurav <agaurav77@yahoo.com>
2019-10-07 03:05:35 -0400

    PriorityManager: add does_anyone_have_priority, misc

--- wm2/priority.py
+++ wm2/priority.py
@@ -12,6 +12,13 @@ class PriorityManager:
         ret_val = self.current_owner == name
         self.mutex.release()
         return ret_val
+    
+    def does_anyone_have_priority(self):
+        ret_val = False
+        self.mutex.acquire()
+        ret_val = self.current_owner != None
+        self.mutex.release()
+        return ret_val
 
     def request_priority(self, who_is_asking):
         ret_val = False
@@ -20,6 +27,7 @@ class PriorityManager:
             ret_val = True
         elif self.current_owner == None:
             self.current_owner = who_is_asking
+            # print('%s acquire' % self.current_owner)
             ret_val = True
         self.mutex.release()
         return ret_val
@@ -28,6 +36,7 @@ class PriorityManager:
         ret_val = False
         self.mutex.acquire()
         if self.current_owner == who_has_it:
+            # print('%s release' % self.current_owner)
             self.current_owner = None
             ret_val = True
         self.mutex.release()
