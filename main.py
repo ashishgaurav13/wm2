2533f7ecaff1735658f8a6d741127354c3318404
Ashish Gaurav <agaurav77@yahoo.com>
2019-10-06 20:45:51 -0400

    Car: add aggresive driving policy, provide example

--- main.py
+++ main.py
@@ -3,21 +3,45 @@ import graphics
 import wm2
 import wmath
 
-canvas = graphics.Canvas(600, 600)
-canvas.set_origin(300, 300)
-canvas.set_scale(600 / 100) # 100m = 600pix
+canvas = graphics.Canvas(600, 600, ox = 300, oy = 300, scale = 600/100)
 canvas.add_static_elements(
     ['Grass'],
     ['TwoLaneRoad', -50, -5, -5, +5, 0.5],
-    ['StopRegion', -10, -5, -5, +5],
     ['TwoLaneRoad', +5, +50, -5, +5, 0.5],
     ['TwoLaneRoad', -5, +5, -50, -5, 0.5],
     ['TwoLaneRoad', -5, +5, +5, +50, 0.5],
     ['Intersection', -5, +5, -5, +5],
+    ['StopRegionX', -10, -5, -5, 0],
+    ['StopRegionX', +5, +10, 0, +5],
+    ['StopRegionY', -5, 0, +5, +10],
+    ['StopRegionY', 0, +5, -10, -5],
 )
-# canvas.show_allowed_regions()
+
+# Add agents
 canvas.add_agents(
-    ['Ego', -45, -2.5, 0.0, wmath.Direction2D(mode = '+x')],
+    ['Veh', -45, -2.5, 0.0, wmath.Direction2D(mode = '+x')],
+    ['Veh', -15, -2.5, 0.0, wmath.Direction2D(mode = '+x')],
+    ['Veh', +45, +2.5, 0.0, wmath.Direction2D(mode = '-x')],
+    ['Veh', +15, +2.5, 0.0, wmath.Direction2D(mode = '-x')],
+    ['Veh', +2.5, -45, 0.0, wmath.Direction2D(mode = '+y')],
+    ['Veh', +2.5, -15, 0.0, wmath.Direction2D(mode = '+y')],
+    ['Veh', -2.5, +45, 0.0, wmath.Direction2D(mode = '-y')],
+    ['Veh', -2.5, +15, 0.0, wmath.Direction2D(mode = '-y')],
 )
-canvas.agents[0].which_regions()
-canvas.render()
+
+# Get acceleration for aggressive driving
+allowed_agents = list(range(8))
+while True:
+    canvas.clear()
+    canvas.switch_to()
+    canvas.dispatch_events()
+
+    for aid, agent in enumerate(canvas.agents):
+        if aid not in allowed_agents: continue
+        control_inputs = agent.aggressive_driving()
+        agent.step(control_inputs)
+
+    drew_agents = canvas.on_draw()
+    canvas.flip()
+
+    # if drew_agents == 0: break
