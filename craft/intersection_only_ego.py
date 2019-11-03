f1dcc7411a07beaf2f5922cdfde22719bdc1e24f
Ashish Gaurav <agaurav77@yahoo.com>
2019-11-03 05:58:51 -0500

    IntersectionOnlyEgoEnv: modify reward structure

--- craft/intersection_only_ego.py
+++ craft/intersection_only_ego.py
@@ -2,6 +2,7 @@ import pyglet
 import tools.pyglet as graphics
 import tools.design as design
 import tools.math as wmath
+import tools.misc as utilities
 import numpy as np
 
 class IntersectionOnlyEgoEnv(design.Environment):
@@ -13,21 +14,22 @@ class IntersectionOnlyEgoEnv(design.Environment):
     Definitions:
         mid_lane: -2.5
         deviation_mid_lane: abs(p['mid_lane'] - p['ego'].f['y'])
+        moving_horizontally: lambda p, t: -5 <= p['ego'].f['y'] <= 5
 
     Propositions:
         stopped: p['ego'].f['v'] == 0,
-        out_of_bounds: not p['ego'].any_regions(),
+        out_of_bounds: (not p['ego'].any_regions()) or (not p['moving_horizontally']),
         near_goal: p['ego'].Lp(+45, -2.5) <= 2.5,
 
     Reward:
-        true, 10 * np.exp(-p['deviation_mid_lane']) (S)
+        true, -p['deviation_mid_lane']/5 (S)
+        stopped, -1 (S)
 
     Termination:
-        stopped, -1 (S)
-        out_of_bounds, -1 (S)
+        out_of_bounds, -100 (S)
     
     Success:
-        near_goal, 1 (S)
+        near_goal, 100 (S)
     """
 
     def __init__(self):
@@ -56,21 +58,32 @@ class IntersectionOnlyEgoEnv(design.Environment):
         # Reward structure
         d = {
             "mid_lane": -2.5,
-            "deviation_mid_lane": lambda p, t: abs(p['mid_lane']-p['ego'].f['y'])
+            "deviation_mid_lane": lambda p, t: abs(p['mid_lane']-p['ego'].f['y']),
+            "moving_horizontally": lambda p, t: -5 <= p['ego'].f['y'] <= 5
         }
         p = {
             "stopped": lambda p, t: p['ego'].f['v'] == 0,
-            "out_of_bounds": lambda p, t: not p['ego'].any_regions(),
+            "out_of_bounds": lambda p, t: (not p['ego'].any_regions()) or (not p['moving_horizontally']),
             "near_goal": lambda p, t: p['ego'].Lp(45, -2.5) <= 2.5,
         }
+        # 0 <= d <= 2.5
+        # 0 <= ed-1 <= 11
         r = [
-            ["true", lambda p, t: 10 * np.exp(-p['deviation_mid_lane']), 'satisfaction']
+            ["true", lambda p, t: -p['deviation_mid_lane']/5, 'satisfaction'],
+            ["stopped", -1, 'satisfaction'],
         ]
         t = [
-            ["stopped", -1, 'satisfaction'],
-            ['out_of_bounds', -1, 'satisfaction'],
+            ['out_of_bounds', -100, 'satisfaction'],
         ]
         s = [
-            ['near_goal', 1, 'satisfaction'],
+            ['near_goal', 100, 'satisfaction'],
         ]
-        self.reward_structure(d, p, r, t, s, round_to = 3, clip_to = [-1, 1])
+        self.reward_structure(d, p, r, t, s, round_to = 3)
+
+        # Specify state
+        ego_fn = lambda agent, rs: utilities.combine_dicts(agent.f.get_dict(), rs._p.get_dict())
+        other_fn = lambda agent, rs: agent.f.get_dict()
+        self.specify_state(ego_fn, other_fn)
+
+        # Finally
+        self.make_ready()
